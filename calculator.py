896d275d9bb23af5b7aa1b25d6b6534c70f0c73b
arbubzik <122701295+arbubzik@users.noreply.github.com>
2023-11-02 20:23:33 +0100

    Update calculator.py
    Adding first button

--- calculator.py
+++ calculator.py
@@ -3,6 +3,14 @@ from customtkinter import *
 #shaping the app window
 app = CTk()
 app.geometry("400x600")
+app.title("Calculator")
 
+set_default_color_theme("dark-blue")
+#button
+
+btn = CTkButton(master=app, width=87, height=63, corner_radius=32, text="(")
+
+btn.pack(pady=20, padx=20)
+btn.place(x = 21, y = 203)
 #starting the app
 app.mainloop()
